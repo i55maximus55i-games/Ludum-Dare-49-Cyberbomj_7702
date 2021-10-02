10e61d730df3ef2e924b162078b4fee2ae2df1a2
Anton Petrochenko <petrochenko.ai@ubmail.ru>
2021-10-02 16:45:27 +0600

    asd;lfjhas

--- factories/player.lua
+++ factories/player.lua
@@ -5,11 +5,14 @@ return function (joyrecord,x,y)
     player.x = x
     player.y = y
     player.tt = 0
+    player.left = false
+
     function player.update(self,dt)
         self.tt = self.tt + dt
         local ax1, ax2, ax3, ax4 = self.joy:getAxes()
         self.x = self.x + ax1
         self.y = self.y + ax2
+        self.left = ax1 < 0
     end
     function player.draw(self,dt)
         love.graphics.circle("fill", self.x, self.y, 16,16)
