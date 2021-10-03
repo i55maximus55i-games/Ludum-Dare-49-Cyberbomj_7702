b36d1eb498f1adf70b313ae28b52d9f5f4147f9b
55_maximus_55 <lukinosh@gmail.com>
2021-10-03 14:24:10 +0600

    hit states

--- factories/punchable.lua
+++ factories/punchable.lua
@@ -16,39 +16,19 @@ return function (x,y)
         punchable:setstate("hit1")
     end)
 
-    punchable.update_states = {}
-    punchable.draw_states = {}
+    punchable.update_states = sharedstates.create_update_states()
+    punchable.draw_states = sharedstates.create_draw_states()
 
     ------ UPDATE STATES
     function punchable.update_states.normal(self, dt)
 
     end
 
-    function punchable.update_states.hit1(self,dt)
-        self.x = self.x + dt*30
-        if self.statetimer > 0.1 then
-            self:setstate("hit2")
-        end
-    end
 
-    function punchable.update_states.hit2(self)
-        if self.statetimer > 0.1 then
-            punchable.hitbox.enabled = true
-            self:setstate("normal")
-        end
-    end
 
     ------ DRAW STATES
     function punchable.draw_states.normal(self,x,y)
         love.graphics.draw(self.frames.idle,x,y)
-    end
-
-    function punchable.draw_states.hit1(self,x,y)
-        love.graphics.draw(self.frames.hit,x,y)
-    end
-
-    function punchable.draw_states.hit2(self,x,y)
-        love.graphics.draw(self.frames.hit,x,y)
     end
 
 
@@ -61,7 +41,7 @@ return function (x,y)
             self.current_update_state = self.update_states[newstate] 
             self.current_draw_state = self.draw_states[newstate]
         else 
-            print("BAD STATAE " .. newstate)
+            print("BAD STATE " .. newstate)
         end
     end
 
--- sharedstates.lua
+++ sharedstates.lua
@@ -5,6 +5,7 @@ sharedstates.draw_states = {}
 
 function sharedstates.create_update_states()
     local new_update_states = {}
+
     function new_update_states.punch1(self)
         if self.statetimer > 0.1 then
             self:setstate("punch2")
@@ -20,6 +21,21 @@ function sharedstates.create_update_states()
             self:setstate("normal")
         end
     end
+
+    function new_update_states.hit1(self,dt)
+        self.x = self.x + dt*30
+        if self.statetimer > 0.1 then
+            self:setstate("hit2")
+        end
+    end
+    function new_update_states.hit2(self)
+        if self.statetimer > 0.1 then
+            self.hitbox.enabled = true
+            self:setstate("normal")
+        end
+    end
+
+
     return new_update_states
 end
 
@@ -32,6 +48,13 @@ function sharedstates.create_draw_states()
     function new_draw_states.punch2(self,dx,dy,dz,f,ox)
         love.graphics.draw(self.frames.punch2, dx, dy - dz,nil,f,1,ox)
     end
+    function new_draw_states.hit1(self,x,y)
+        love.graphics.draw(self.frames.hit,x,y)
+    end
+    function new_draw_states.hit2(self,x,y)
+        love.graphics.draw(self.frames.hit,x,y)
+    end
+    
     return new_draw_states
 end
 
