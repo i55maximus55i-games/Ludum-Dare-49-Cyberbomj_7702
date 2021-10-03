d4637955264945249e5d79378ca4080c633b3eb1
55_maximus_55 <lukinosh@gmail.com>
2021-10-03 09:32:01 +0600

    hit sound

--- sounds.lua
+++ sounds.lua
@@ -1,2 +1,3 @@
 sound_player_join = love.audio.newSource("/assets/audio/player-join.wav", "static")
 sound_player_disconnect = love.audio.newSource("/assets/audio/player-disconnect.wav", "static")
+sound_hit = love.audio.newSource("/assets/audio/hit.wav", "static")
