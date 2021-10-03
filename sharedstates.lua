544a905634e15c6696992dd217df7c0f5234d113
55_maximus_55 <lukinosh@gmail.com>
2021-10-03 14:37:47 +0600

    all attack states

--- sharedstates.lua
+++ sharedstates.lua
@@ -6,22 +6,6 @@ sharedstates.draw_states = {}
 function sharedstates.create_update_states()
     local new_update_states = {}
 
-    function new_update_states.punch1(self)
-        if self.statetimer > 0.1 then
-            self:setstate("punch2")
-        end
-    end
-    function new_update_states.punch2(self)
-        if self.left then
-            hitbox.tryhit(self, self.x, self.y+12, self.z, 5, 5, 5, {-5, 0, 5})            
-        else
-            hitbox.tryhit(self, self.x+19, self.y+12, self.z, 5, 5, 5, {5, 0, 5})
-        end
-        if self.statetimer > 0.1 then
-            self:setstate("normal")
-        end
-    end
-
     function new_update_states.hit1(self,dt)
         self.x = self.x + dt*30
         if self.statetimer > 0.1 then
@@ -35,6 +19,69 @@ function sharedstates.create_update_states()
         end
     end
 
+    function new_update_states.punch1(self)
+        if self.statetimer > 0.1 then
+            self:setstate("punch2")
+        end
+    end
+    function new_update_states.punch2(self)
+        if self.left then
+            hitbox.tryhit(self, self.x, self.y+12, self.z, 5, 5, 5, {-5, 0, 0})            
+        else
+            hitbox.tryhit(self, self.x+19, self.y+12, self.z, 5, 5, 5, {5, 0, 0})
+        end
+        if self.statetimer > 0.1 then
+            self:setstate("normal")
+        end
+    end
+
+    function new_update_states.uppercut1()
+        if self.statetimer > 0.1 then
+            self:setstate("uppercut2")
+        end
+    end
+    function new_update_states.uppercut2()
+        if self.left then
+            hitbox.tryhit(self, self.x, self.y+12, self.z, 5, 5, 5, {-5, 0, 15})            
+        else
+            hitbox.tryhit(self, self.x+19, self.y+12, self.z, 5, 5, 5, {5, 0, 15})
+        end
+        if self.statetimer > 0.1 then
+            self:setstate("normal")
+        end
+    end
+
+    function new_update_states.kick1()
+        if self.statetimer > 0.1 then
+            self:setstate("kick2")
+        end
+    end
+    function new_update_states.kick2()
+        if self.left then
+            hitbox.tryhit(self, self.x, self.y+12, self.z, 5, 5, 5, {-5, 0, 0})            
+        else
+            hitbox.tryhit(self, self.x+19, self.y+12, self.z, 5, 5, 5, {5, 0, 0})
+        end
+        if self.statetimer > 0.1 then
+            self:setstate("normal")
+        end
+    end
+
+    function new_update_states.elbow1()
+        if self.statetimer > 0.1 then
+            self:setstate("elbow2")
+        end
+    end
+    function new_update_states.elbow2()
+        if self.left then
+            hitbox.tryhit(self, self.x+19, self.y+12, self.z, 5, 5, 5, {5, 0, 0})
+        else
+            hitbox.tryhit(self, self.x, self.y+12, self.z, 5, 5, 5, {-5, 0, 0})            
+        end
+        if self.statetimer > 0.1 then
+            self:setstate("normal")
+        end
+    end
 
     return new_update_states
 end
@@ -42,17 +89,40 @@ end
 
 function sharedstates.create_draw_states()
     local new_draw_states = {}
+    
+    function new_draw_states.hit1(self,dx,dy,dz,f,ox)
+        love.graphics.draw(self.frames.hit,dx, dy - dz,nil,f,1,ox)
+    end
+    function new_draw_states.hit2(self,dx,dy,dz,f,ox)
+        love.graphics.draw(self.frames.hit,dx, dy - dz,nil,f,1,ox)
+    end
+    
     function new_draw_states.punch1(self,dx,dy,dz,f,ox)
         love.graphics.draw(self.frames.punch1, dx, dy - dz,nil,f,1,ox)
     end
     function new_draw_states.punch2(self,dx,dy,dz,f,ox)
         love.graphics.draw(self.frames.punch2, dx, dy - dz,nil,f,1,ox)
+    end    
+
+    function new_draw_states.uppercut1(self,dx,dy,dz,f,ox)
+        love.graphics.draw(self.frames.uppercut1, dx, dy - dz,nil,f,1,ox)
     end
-    function new_draw_states.hit1(self,x,y)
-        love.graphics.draw(self.frames.hit,x,y)
+    function new_draw_states.uppercut2(self,dx,dy,dz,f,ox)
+        love.graphics.draw(self.frames.uppercut2, dx, dy - dz,nil,f,1,ox)
+    end    
+
+    function new_draw_states.kick1(self,dx,dy,dz,f,ox)
+        love.graphics.draw(self.frames.kick1, dx, dy - dz,nil,f,1,ox)
     end
-    function new_draw_states.hit2(self,x,y)
-        love.graphics.draw(self.frames.hit,x,y)
+    function new_draw_states.kick2(self,dx,dy,dz,f,ox)
+        love.graphics.draw(self.frames.kick2, dx, dy - dz,nil,f,1,ox)
+    end
+        
+    function new_draw_states.elbow1(self,dx,dy,dz,f,ox)
+        love.graphics.draw(self.frames.elbow1, dx, dy - dz,nil,f,1,ox)
+    end
+    function new_draw_states.elbow2(self,dx,dy,dz,f,ox)
+        love.graphics.draw(self.frames.elbow2, dx, dy - dz,nil,f,1,ox)
     end
     
     return new_draw_states
--- sounds.lua
+++ sounds.lua
@@ -1,3 +1,7 @@
 sound_player_join = love.audio.newSource("/assets/audio/player-join.wav", "static")
 sound_player_disconnect = love.audio.newSource("/assets/audio/player-disconnect.wav", "static")
 sound_hit = love.audio.newSource("/assets/audio/hit.wav", "static")
+
+music_boiled_piss = love.audio.newSource("/assets/audio/Boiled piss.wav", "stream")
+music_hobo = love.audio.newSource("/assets/audio/Hobo is shitting.wav", "stream")
+music_shit = love.audio.newSource("/assets/audio/Piece of shit.wav", "stream")
